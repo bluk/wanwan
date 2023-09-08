fee42eafa292d4a10848a18e36f4049b8524f166
Bryant Luk <code@bryantluk.com>
2023-09-08 16:13:14 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+//! A WebAssembly interpreter.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![warn(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    rust_2018_idioms,
+    unused_lifetimes,
+    unused_qualifications
+)]
+
+#[cfg(all(feature = "alloc", not(feature = "std")))]
+extern crate alloc;
